5d52a306b1a1df16cad39dba319604d1d1ed00ac
bsteele-dev <brian.steele@salesforce.com>
2022-12-03 16:30:20 -0500

    huge refactor

--- AOC_2.php
+++ AOC_2.php
@@ -2500,16 +2500,18 @@ A Y
 B Y
 A Y';
 
+$testInput = 'A Y
+B X
+C Z';
+$testScore = 15;
+
 /**
  *Your total score is the sum of your scores for each round.
  * The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
  * plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
  */
 
-$testInput = 'A Y
-B X
-C Z';
-$testScore = 15;
+
 
 $inputMovesArray = str_split($puzzleInput);
 
