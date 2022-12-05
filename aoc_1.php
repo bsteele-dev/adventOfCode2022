fce88ee6377150012b79a0031c021d57bd4e3f44
bsteele-dev <brian.steele@salesforce.com>
2022-12-05 16:57:40 -0500

    WAIT THERES A PART TWO?! FUUUUUUUUUUUUUUUCK. Solve for day 1 PART TWO and add test input

--- aoc_1.php
+++ aoc_1.php
@@ -2,16 +2,17 @@
 $elfCaloriesString = file_get_contents('./input/day1.txt');
 
 $arrayOfElfCalories = explode("\n", $elfCaloriesString);
-$maxCalories = 0;
+$maxCalories = [];
 $tempSum = 0;
 foreach ($arrayOfElfCalories as $caloricEntry => $value) {
 	$tempSum += (int)$value;
 	if (empty($value)) {
-		if ($tempSum >= $maxCalories) {
-			$maxCalories = $tempSum;
+		if ($tempSum >= $maxCalories[0]) {
+			array_unshift($maxCalories, $tempSum);
 		}
 		$tempSum = 0;
 	}
 }
 
-print "$maxCalories\n";
+print "Highest Calorie Count: $maxCalories[0]\n";
+print "Sum of Calories of Top 3 Elves: " . ($maxCalories[0] + $maxCalories[1] + $maxCalories[2]) . "\n";
